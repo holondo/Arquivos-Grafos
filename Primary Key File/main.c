f77bf7b121316c4960f2ba807aea84e647b76f0e
holondo <holondo@outlook.com>
2021-05-05 19:15:33 -0300

    First implementation of the required menu

---
+++ Primary Key File/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include "student.h"
+
+#define FILE_NAME "rec.bin"
+
+/*int getArg(char strArg[MAX_STRING])
+{
+    char aux = '\0';
+    int i = 0;
+    while (1)
+    {
+        aux = getchar();
+        if(aux == EOF && i == 0) return ERROR_;
+        if(aux == ',' || aux == ';' || aux == '\n' || aux == EOF)//Se for final do campo
+        {
+            strArg[i] = '\0'; //Finaliza string        
+            break;
+        }
+        strArg[i] = aux;
+        i++;
+    }
+
+    if(aux == EOF) return ALERT_;
+    return SUCCESS_;
+}*/
+
+void menu(FILE* data)
+{
+    char strArg[MAX_STRING];
+
+    while (scanf("%d ", strArg))
+    {
+        if(strcmp(strArg, "insert"))
+        {
+            //lineToRecord(data);
+        }
+
+        if(strcmp(strArg, "search"))
+        {
+            
+        }
+
+        if(strcmp(strArg, "delete"))
+        {
+            
+        }
+
+        if(strcmp(strArg, "exit")) return;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    FILE *data;
+    data = fopen(FILE_NAME, "w+");
+
+    menu(data);
+    return 0;
+}
